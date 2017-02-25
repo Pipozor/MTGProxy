6a65a5e912160c0320b6c623bd06f49b052409dc
abraxas27 <abraxas27@hotmail.fr>
2017-02-25 20:43:50 +0100

    Parameteres in an .ini file + dual search mode

--- MTGProxy.py
+++ MTGProxy.py
@@ -11,12 +11,16 @@ ROOT_DIR = os.path.realpath(os.path.join(os.path.dirname(__file__)))
 INPUT_REGEX = "^(?:SB\s?\:\s?)?(\d+)\s+(?:\[(.{2}.?)\])?\s?([\w\,\'\-\s]*?)(?:/+[\w\,\'\-\s]*)?$"
 ONLINE_REGEX = "(http://magiccards\.info/scans/en/\w{2,3}/\d{1,3}\w?\.jpg)"
 
-output_dir = os.path.join(ROOT_DIR, "output", "Proxy")
+current_proxy_id = 1
+output_path = os.path.join(ROOT_DIR, "output", "Proxy")
+output_dir = output_path
 not_found_file = os.path.join(ROOT_DIR, "input", "not_found.txt")
-input_file = os.path.join(ROOT_DIR, "input", "Proxy.txt")
+proxy_file = os.path.join(ROOT_DIR, "input", "Proxy.txt")
 edition_file = os.path.join(ROOT_DIR, "conf", "Edition.txt")
-scans_dir = ''
+scans_dir = os.path.join(ROOT_DIR, "scans")
 online_mode = True
+offline_mode = True
+mode_priority = "online"
 
 # TODO: support special-encoded character, like 'æ'
 # encoding_map = {u'æ': u'ae'}
@@ -29,8 +33,8 @@ online_mode = True
 # TODO: generate PDF files
 
 
-def findcard(loc, name, searchdir=None):
-    regex = name.lower()
+def findcardoffline(name, searchdir=None):
+    regex = name.lower() + '(?! )\.'  # not followed by a space, but followed by a dot
     found = ''
     # print("regex : '{}' ".format(regex))
     with open(edition_file, 'r', encoding='utf8') as filedesc:
@@ -40,16 +44,17 @@ def findcard(loc, name, searchdir=None):
         if re.match("\d{8}\t(.{3})\t", edition_line):
             trigram = re.findall("\d{8}\t(.{3})\t", edition_line)[0]
             # print(trigram)
-            edition_scan_path = os.path.join(loc, trigram)
+            edition_scan_path = os.path.join(scans_dir, trigram)
             # print(edition_scan_path + " test")
             if os.path.exists(edition_scan_path):
                 for filename in os.listdir(edition_scan_path):
-                    edition_file_path = os.path.join(edition_scan_path, filename)
-                    if re.match(regex, filename, re.IGNORECASE) and os.path.getsize(edition_file_path) > 0:
-                        print(edition_file_path)
-                        found = edition_file_path
-                        if not searchdir or os.path.basename(edition_scan_path) == searchdir:
-                            return found
+                    if re.match(regex, filename, re.IGNORECASE):
+                        edition_file_path = os.path.join(edition_scan_path, filename)
+                        if os.path.getsize(edition_file_path) > 0:
+                            # print(edition_file_path)
+                            found = edition_file_path
+                            if not searchdir or os.path.basename(edition_scan_path) == searchdir:
+                                return found
     if found:
         return found
 
@@ -64,15 +69,7 @@ def card_not_found(name):
 
 
 def init():
-    if not os.path.isfile(input_file):
-        sys.exit("Provided input file is not valid")
-    if online_mode is False:
-        if not scans_dir:
-            print("Ref option is mandatory in offline mode")
-            usage()
-            sys.exit(1)
-        elif not os.path.isdir(scans_dir):
-            sys.exit("Offline mode requires valid scans folder")
+
     # All is OK !
     if os.path.isdir(output_dir):
         # Delete previous work and init current one
@@ -90,78 +87,127 @@ def usage():
           .format(os.path.basename(sys.argv[0])))
 
 
-def process_param():
+def get_program_param():
     config = configparser.ConfigParser()
     config.read(os.path.join(ROOT_DIR, 'conf', 'global.ini'))
-    global input_file
+    global proxy_file
     global output_dir
+    global output_path
     global online_mode
     global scans_dir
-
-    input_file = config.get('input', 'proxy_list')
-    if not os.path.isabs(input_file):
-        input_file = os.path.realpath(os.path.join(ROOT_DIR, input_file))
-
+    global offline_mode
+    global mode_priority
+    # Proxy input file
+    proxy_file = config.get('input', 'proxy_list')
+    if not os.path.isabs(proxy_file):
+        proxy_file = os.path.realpath(os.path.join(ROOT_DIR, proxy_file))
+    if not os.path.isfile(proxy_file):
+        sys.exit(proxy_file + "is not a valid file")
+    # Output directory
     output_path = config.get('output', 'output_path')
     output_dir = output_path
     if not os.path.isabs(output_dir):
         output_dir = os.path.realpath(os.path.join(ROOT_DIR, output_dir, 'Proxy'))
-    # config.getboolean('mode', 'online_mode')
-    if config.get('mode', 'mode') == "online":
-        online_mode = True
-    elif config.get('mode', 'mode') == "offline":
-        online_mode = False
-
+    # Search modes
+    offline_mode = config.getboolean('mode', 'offline_mode')
+    online_mode = config.getboolean('mode', 'online_mode')
+    mode_priority = config.get('mode', 'mode_priority')
+    # Scans directory (offline mode)
     scans_dir = config.get('input', 'scans_directory')
     if not os.path.isabs(scans_dir):
         scans_dir = os.path.realpath(os.path.join(ROOT_DIR, scans_dir))
+    if offline_mode and (not scans_dir or not os.path.isdir(scans_dir)):
+        print("Scans directory muste be defined and valid for offline mode")
+
+
+def copy_card(path, quantity):
+    global current_proxy_id
+    for i in range(0, int(quantity)):
+        shutil.copy(path, os.path.join(output_dir, str(current_proxy_id) + ".jpg"))
+        current_proxy_id += 1
+
+
+def create_proxy_online(quantity, cardname, cardset=''):
+    global current_proxy_id
+
+    # print("quantity : " + quantity)
+    # print("cardset : " + cardset)
+    if cardset == '':
+        searchurl = "http://magiccards.info/query?q=!{}&v=card&s=cname".format(
+            quote_plus(cardname.lower()))
+    else:
+        searchurl = "http://magiccards.info/query?q=!{}+e%3A{}&v=card&s=cname".format(
+            quote_plus(cardname.lower()),
+            quote_plus(cardset.lower()))
+    # print("Search with : {}".format(searchurl))
+    with urlopen(searchurl) as response:
+        httpcontent = response.readlines()
+    if re.search(ONLINE_REGEX, str(httpcontent)):
+        imageurl = re.findall(ONLINE_REGEX, str(httpcontent))[0]
+        print("--> {} '{}' created using {}".format(quantity, cardname, imageurl))
+        tmpfile = urlretrieve(imageurl)[0]
+        copy_card(tmpfile, quantity)
+        for i in range(0, int(quantity)):
+            current_proxy_id += 1
+            shutil.copy(tmpfile, os.path.join(output_dir, str(current_proxy_id) + ".jpg"))
+        return 1
+    else:
+        print("--> '" + cardname + "' not found online with URL " + searchurl)
+        return 0
+
+
+def create_proxy_offline(quantity, cardname, cardset=''):
+    filepath = findcardoffline(cardname, cardset)
+    # print(filepath)
+    if filepath is not None:
+        print("--> {} '{}' created using file {}".format(quantity, cardname, filepath))
+        copy_card(filepath, quantity)
+        return 1
+    else:
+        print("'{}' not found in scans directory ".format(cardname))
+        return 0
+
+
+def process_input_file(input_file):
+    with open(input_file, 'r', encoding='utf8') as fd:
+        for line in fd:
+            # Test line format
+            if re.match(INPUT_REGEX, line, re.M | re.I) is not None:
+                quantity, cardset, cardname = re.findall(INPUT_REGEX, line, re.M | re.I)[0]
+                quantity = quantity.strip()
+                cardset = cardset.strip()
+                cardname = cardname.strip()
+                # print("quantity : " + quantity)
+                # print("cardset : " + cardset)
+                if cardset == '':
+                    print("Searching for '{}' in the most recent edition".format(cardname))
+                else:
+                    print("Searching for '{}' in [{}] edition".format(cardname, cardset))
+                if online_mode and offline_mode:
+                    if mode_priority.lower() == "online":
+                        if not create_proxy_online(quantity, cardname, cardset):
+                            if not create_proxy_offline(quantity, cardname, cardset):
+                                card_not_found(line)
+                    elif mode_priority.lower() == 'offline':
+                        if not create_proxy_offline(quantity, cardname, cardset):
+                            if not create_proxy_online(quantity, cardname, cardset):
+                                card_not_found(line)
+                    else:
+                        print("No mode priority defined, exiting...")
+                        sys.exit(2)
+                elif online_mode:
+                    if not create_proxy_online(quantity, cardname, cardset):
+                        card_not_found(line)
+                elif offline_mode:
+                    if not create_proxy_offline(quantity, cardname, cardset):
+                        card_not_found(line)
+                else:
+                    print("No mode selected, exiting...")
+                    sys.exit(2)
 
 
 if __name__ == '__main__':
-    # process_opts()
-    process_param()
-
+    get_program_param()
     init()
-    placeholderid = 0
     # Process input file
-    with open(input_file, 'r', encoding='utf8') as fd:
-        for line in fd:
-            if re.match(INPUT_REGEX, line, re.M | re.I) is not None:
-                quantity, cardset, cardname = re.findall(INPUT_REGEX, line, re.M | re.I)[0]
-                # print("quantity : " + quantity)
-                # print("cardset : " + cardset)
-                if online_mode is False:
-                    location = scans_dir
-                    filepath = findcard(location, cardname, searchdir=cardset)
-                    # print(filepath)
-                    if filepath is not None:
-                        print("Create {} {}".format(quantity, os.path.basename(filepath)))
-                        for i in range(0, int(quantity)):
-                            placeholderid += 1
-                            shutil.copy(filepath, os.path.join(output_dir, str(placeholderid) + ".jpg"))
-                    else:
-                        print("Not found : {}".format(cardname))
-                        card_not_found(line)
-                elif online_mode is True:
-                    searchurl = ''
-                    if cardset == '':
-                        searchurl = "http://magiccards.info/query?q=!{}&v=card&s=cname".format(
-                            quote_plus(cardname.lower()))
-                    else:
-                        searchurl = "http://magiccards.info/query?q=!{}+e%3A{}&v=card&s=cname".format(
-                            # searchurl = "http://magiccards.info/query?q=!{}+e%3A{}%2Fen&v=card&s=cname".format(
-                            quote_plus(cardname.lower()),
-                            quote_plus(cardset.lower()))
-                    with urlopen(searchurl) as response:
-                        httpcontent = response.readlines()
-                    # urlretrieve(searchurl, cardname+".txt")
-                    if re.search(ONLINE_REGEX, str(httpcontent)):
-                        imageurl = re.findall(ONLINE_REGEX, str(httpcontent))[0]
-                        print("Create {} {} using : {}".format(quantity, os.path.basename(cardname), imageurl))
-                        tmpfile = urlretrieve(imageurl)[0]
-                        for i in range(0, int(quantity)):
-                            placeholderid += 1
-                            shutil.copy(tmpfile, os.path.join(output_dir, str(placeholderid) + ".jpg"))
-                    else:
-                        print(cardname + " not found with : " + searchurl)
-                        card_not_found(line)
+    process_input_file(proxy_file)
